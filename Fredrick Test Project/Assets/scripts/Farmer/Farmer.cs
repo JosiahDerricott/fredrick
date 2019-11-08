43a31224ba9a5c1d7656b61db2e0103e159abbd5
DubrayMa <46659541+DubrayMa@users.noreply.github.com>
2019-11-08 13:26:18 -0800

    Implements sight detection mechanic
    farmer moves through a path that is pre generated, and has a spotlight to show range of sight, and debug.logs if it sees the player or not, player can hide behind obsticles that obstruct farmers view

---
+++ Fredrick Test Project/Assets/scripts/Farmer/Farmer.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Farmer : MonoBehaviour
+{
+    /** DOCUMENTATION
+     * p_(var name) indictates a private non viewed variable within the Inspector
+     * _(var name) indicates a variable that can both be viewed and changed within the Inspector
+     * m_(var name) indicates components it is refrencing to in the script
+     * 
+     */
+
+
+
+    public Transform m_path;
+    public Light m_spotLight;
+    public Transform m_player;
+    public LayerMask m_viewMask;
+
+    public float _moveSpd = 20f;
+    public float _waitTime = .5f;
+    public float _turnSpd = 180f;
+    public float _viewDist;
+
+
+    float _viewAngle;
+
+    void Start() {
+        m_player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        _viewAngle = m_spotLight.spotAngle;
+
+        Vector3[] p_pathPoints = new Vector3[m_path.childCount];
+        for(int i = 0; i < p_pathPoints.Length; i++) {
+            p_pathPoints[i] = m_path.GetChild(i).position;
+            p_pathPoints[i] = new Vector3(p_pathPoints[i].x, transform.position.y, p_pathPoints[i].z);
+        }
+        StartCoroutine(followPath(p_pathPoints));
+    }
+    // Update is called once per frame
+    void Update() {
+        if(playerInSight()) {
+            Debug.Log("Player is visable");
+        } else
+        {
+            Debug.Log("Player is not visable");
+        }
+    }
+
+    IEnumerator followPath(Vector3[] p_pathPoints) {
+
+        transform.position = p_pathPoints[0];
+
+        int p_nextPointIndex = 1;
+        Vector3 p_targetPoint = p_pathPoints[p_nextPointIndex];
+        transform.LookAt(p_targetPoint);
+
+        while (true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, p_targetPoint, _moveSpd * Time.deltaTime);
+            if (transform.position == p_targetPoint)
+            {
+                p_nextPointIndex = (p_nextPointIndex + 1) % p_pathPoints.Length;
+                p_targetPoint = p_pathPoints[p_nextPointIndex];
+                yield return new WaitForSeconds(_waitTime);
+                yield return StartCoroutine(turnForward(p_targetPoint));
+            }
+            yield return null;
+        }
+    }
+    IEnumerator turnForward(Vector3 p_lookTarget) {
+
+        Vector3 p_dirToLookTarget = (p_lookTarget - transform.position).normalized;
+        float p_targetAngle = 90 - Mathf.Atan2(p_dirToLookTarget.z, p_dirToLookTarget.x) * Mathf.Rad2Deg;
+
+        while(Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, p_targetAngle)) > 0.05f) {
+            float p_angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, p_targetAngle, _turnSpd * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * p_angle;
+            yield return null;
+        }
+    }
+
+    bool playerInSight() {  
+        if (Vector3.Distance(transform.position, m_player.position) < _viewDist) {
+            Vector3 p_playerAngle = (m_player.position - transform.position).normalized;
+            float p_angleBetweenFarmer_Player = Vector3.Angle(transform.forward, p_playerAngle);
+            if(p_angleBetweenFarmer_Player < _viewAngle/2f) { 
+                if(!Physics.Linecast(transform.position, m_player.position, m_viewMask)) {
+                    return true;
+                }
+
+            }
+
+        }
+        return false;
+    }
+
+    
+
+    private void OnDrawGizmos() {
+        Vector3 p_startPos = m_path.GetChild(0).position;
+        Vector3 p_previousPos = p_startPos;
+        foreach (Transform p_pathPoint in m_path) {
+            Gizmos.DrawSphere(p_pathPoint.position, 1f);
+            Gizmos.DrawLine(p_previousPos, p_pathPoint.position);
+            p_previousPos = p_pathPoint.position;
+        }
+        Gizmos.DrawLine(p_previousPos, p_startPos);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay (transform.position, transform.forward * _viewDist);
+    }
+}
---
+++ Fredrick Test Project/Assets/scripts/Player/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /** DOCUMENTATION 
+     * p_(var name) indictates a private non viewed variable within the Inspector
+     * _(var name) indicates a variable that can both be viewed and changed within the Inspector
+     * m_(var name) indicates components it is refrencing to in the script
+     * 
+     */
+    public float _moveSpd = 21f;
+    public float _smoothMoveTime = .1f;
+    public float _turnSpd = 8f;
+
+    float p_smoothInputMag;
+    float p_smoothMoveVel;
+    float p_angle;
+    Vector3 p_velocity;
+
+    Rigidbody m_rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 p_inputDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float p_inputMag = p_inputDir.magnitude;
+        p_smoothInputMag = Mathf.SmoothDamp(p_smoothInputMag, p_inputMag, ref p_smoothMoveVel, _smoothMoveTime);
+
+        float p_targetAngle = Mathf.Atan2(p_inputDir.x, p_inputDir.z) * Mathf.Rad2Deg;
+        p_angle = Mathf.LerpAngle(p_angle, p_targetAngle, Time.deltaTime * _turnSpd * p_inputMag);
+
+        p_velocity = transform.forward * _moveSpd * p_smoothInputMag;
+    }
+
+    void FixedUpdate() {
+        m_rb.MoveRotation(Quaternion.Euler(Vector3.up * p_angle));
+        m_rb.MovePosition(m_rb.position + p_velocity * Time.deltaTime);
+    }
+}
