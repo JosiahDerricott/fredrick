8b15d7b1de967338b7a2e3c67da6b52386e30cf4
Joey <derricott.joey@gmail.com>
2019-12-13 15:09:18 -0800

    Add logic to Quest script

--- Fredrick Test Project/Assets/scripts/Quest/Quest.cs
+++ Fredrick Test Project/Assets/scripts/Quest/Quest.cs
@@ -1,18 +1,91 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class Quest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [System.Serializable]
+    public struct QuestStep
+    {
+        public int _stepNumber;
+        public string _stepDesc;
+    }
+
+    public string _name, _desc;
+
+    // Queue will be more efficient later
+    public List<QuestStep> _currentQuestSteps = new List<QuestStep>();
+
+    // Queue will be more efficient later
+    public List<QuestStep> _completedQuestSteps = new List<QuestStep>();
+
+    bool isComplete;
+
     void Start()
     {
-        
+        /* Once we implement saving, this is field 
+         * that will be serialized and retrieved */
+        isComplete = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetDesc()
+    {
+        return _desc;
+    }
+
+    public bool GetIsComplete()
+    {
+        return isComplete;
+    }
+
+    public void SetName(string newName)
+    {
+        _name = newName;
+    }
+
+    public void SetDesc(string newDesc)
+    {
+        _desc = newDesc;
+    }
+
+    public void SetIsComplete(bool flag)
+    {
+        isComplete = flag;
+    }
+
+    /*
+     * 
+     *  Returns true if step could be completed.
+     *  Returns false if no steps were left to complete.
+     * 
+     */
+    public bool CompleteStep()
+    {
+        if (_currentQuestSteps.Count > 0)
+        {
+            // Create temp of step to be completed
+            QuestStep temp = _currentQuestSteps[0];
+
+            // Remove step
+            _currentQuestSteps.RemoveAt(0);
+
+            // Set as completed
+            _completedQuestSteps.Add(temp);
+
+            return true;
+        }
+
+        return false;
     }
 }
---
+++ Fredrick Test Project/Assets/scripts/Quest/QuestReceiver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestReceiver : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
